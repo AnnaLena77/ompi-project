6726268ed1bf23b9b1a0ac3299bf56e374c14743
Anna-Lena Roth <anna-lena.roth@informatik.hs-fulda.de>
2021-12-14 08:34:02 +0100

    add all blocking and non-blocking operations to queue

--- ompi/mpi/c/isend.c
+++ ompi/mpi/c/isend.c
@@ -33,6 +33,8 @@
 #include "ompi/request/request.h"
 #include "ompi/memchecker.h"
 #include "ompi/runtime/ompi_spc.h"
+#include "ompi/mpi/c/init.h"
+#include <time.h>
 
 #if OMPI_BUILD_MPI_PROFILING
 #if OPAL_HAVE_WEAK_SYMBOLS
@@ -47,6 +49,9 @@ static const char FUNC_NAME[] = "MPI_Isend";
 int MPI_Isend(const void *buf, int count, MPI_Datatype type, int dest,
               int tag, MPI_Comm comm, MPI_Request *request)
 {
+    time_t current_time = time(NULL);
+    char *operation = "isend";
+    enqueue(operation, count*sizeof(type), current_time);
     int rc = MPI_SUCCESS;
 
     SPC_RECORD(OMPI_SPC_ISEND, 1);
