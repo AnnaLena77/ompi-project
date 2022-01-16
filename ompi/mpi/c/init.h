59e37d8d2b5d464a817b64fc706c29d8c9d02014
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2022-01-16 17:23:30 +0100

    sample data with random numbers

--- ompi/mpi/c/init.h
+++ ompi/mpi/c/init.h
@@ -1,4 +1,4 @@
-//#define ENABLE_ANALYSIS 1;
+#define ENABLE_ANALYSIS 1;
 extern void enqueue(char** operation, char** datatype, int count, int datasize, char** communicator, int processrank, int partnerrank, time_t ctime);
 extern void initialize(void);
 extern pthread_t MONITOR_THREAD;
