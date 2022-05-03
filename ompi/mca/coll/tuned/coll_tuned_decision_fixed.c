d59d220a524aa702ffc0d8311a4b207e20b257aa
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2022-05-03 10:40:02 +0200

    fix 1

--- ompi/mca/coll/tuned/coll_tuned_decision_fixed.c
+++ ompi/mca/coll/tuned/coll_tuned_decision_fixed.c
@@ -660,14 +660,16 @@ int ompi_coll_tuned_bcast_intra_dec_fixed(void *buff, int count,
         }
     }
 #ifdef ENABLE_ANALYSIS
-    if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
-    else if(alg == 2) strcpy(item->usedAlgorithm, "chain");
-    else if(alg == 3) strcpy(item->usedAlgorithm, "pipeline");
-    else if(alg == 4) strcpy(item->usedAlgorithm, "split_binary_tree");
-    else if(alg == 5) strcpy(item->usedAlgorithm, "binomial");
-    else if(alg == 6) strcpy(item->usedAlgorithm, "knomial");
-    else if(alg == 7) strcpy(item->usedAlgorithm, "scatter_allgather");
-    else if(alg == 8) strcpy(item->usedAlgorithm, "scatter_allgather_ring");
+    if(item!=NULL){
+        if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
+        else if(alg == 2) strcpy(item->usedAlgorithm, "chain");
+        else if(alg == 3) strcpy(item->usedAlgorithm, "pipeline");
+        else if(alg == 4) strcpy(item->usedAlgorithm, "split_binary_tree");
+        else if(alg == 5) strcpy(item->usedAlgorithm, "binomial");
+        else if(alg == 6) strcpy(item->usedAlgorithm, "knomial");
+        else if(alg == 7) strcpy(item->usedAlgorithm, "scatter_allgather");
+        else if(alg == 8) strcpy(item->usedAlgorithm, "scatter_allgather_ring");
+    }
     return ompi_coll_tuned_bcast_intra_do_this (buff, count, datatype, root,
                                                 comm, module,
                                                 alg, 0, 0, &item);
@@ -1497,9 +1499,11 @@ int ompi_coll_tuned_gather_intra_dec_fixed(const void *sbuf, int scount,
                                                  root, comm, module,
                                                  alg, 0, 0);
 #else
-    if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
-    else if(alg == 2) strcpy(item->usedAlgorithm, "binomial");
-    else if(alg == 3) strcpy(item->usedAlgorithm, "linear_sync");
+    if(item!=NULL){
+        if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
+        else if(alg == 2) strcpy(item->usedAlgorithm, "binomial");
+        else if(alg == 3) strcpy(item->usedAlgorithm, "linear_sync");
+    }
     return ompi_coll_tuned_gather_intra_do_this (sbuf, scount, sdtype,
                                                  rbuf, rcount, rdtype,
                                                  root, comm, module,
@@ -1626,9 +1630,11 @@ int ompi_coll_tuned_scatter_intra_dec_fixed(const void *sbuf, int scount,
                                                   root, comm, module,
                                                   alg, 0, 0);
 #else
-    if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
-    else if(alg == 2) strcpy(item->usedAlgorithm, "binomial");
-    else if(alg == 3) strcpy(item->usedAlgorithm, "linear_nb");
+    if(item!=NULL){
+        if(alg == 1) strcpy(item->usedAlgorithm, "basic_linear");
+        else if(alg == 2) strcpy(item->usedAlgorithm, "binomial");
+        else if(alg == 3) strcpy(item->usedAlgorithm, "linear_nb");
+    }
     return ompi_coll_tuned_scatter_intra_do_this (sbuf, scount, sdtype,
                                                   rbuf, rcount, rdtype,
                                                   root, comm, module,
