465bb67e40f64bd42c757f9607deb4c9ce5e9e6e
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2023-08-08 19:38:12 +0200

    test

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -360,7 +360,7 @@ void writeIntoFile(qentry **q){
         
         int count = item->count;
         if(count>9){
-            int offset2 = log10(count);
+            int offset2 = log10(count)+1;
 	   for(int i = offset2; i>=0; i--){
                 buffer[offset+i] = (count%10) + '0';
                 count = count/10;
