b95cffb05333dadbab581ac2641dcbe9d9357964
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2023-08-09 17:04:30 +0200

    test

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -380,9 +380,9 @@ void writeIntoFile(qentry **q){
             buffer_help[i] = '\0';
             
             while(i>=0){
+                i--;
                 buffer[offset] = buffer_help[i];
                 offset++;
-                i--;
             }
             buffer[offset] = ',';
             offset ++;
