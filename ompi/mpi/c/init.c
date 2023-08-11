5feee72322164162b834b51a5830b51b232d6e08
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2023-08-11 11:31:46 +0200

    add timespec

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -120,7 +120,7 @@ static char *getTimeString(struct timespec time){
     localtime_r(&seconds, &local_time);
     char *timestamp = (char*)malloc(100);
     
-    snprintf(timestamp, 100, "%04d-%02d-%02d %02d:%02d:%02d.%06li", local_time.tm_year + 1900, local_time.tm_mon + 1, local_time.tm_mday, local_time.tm_hour, local_time.tm_min, local_time.tm_sec, nanoseconds);
+    //snprintf(timestamp, 100, "%04d-%02d-%02d %02d:%02d:%02d.%06li", local_time.tm_year + 1900, local_time.tm_mon + 1, local_time.tm_mday, local_time.tm_hour, local_time.tm_min, local_time.tm_sec, nanoseconds);
     
     return timestamp;
 }
