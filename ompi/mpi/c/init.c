bd0097581c0c8dafe9042652329e4d3e5f6f1967
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2023-08-11 09:09:39 +0200

    add timespec

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -120,7 +120,6 @@ static char *getTimeString(struct timespec time){
     snprintf(timestamp, strlen(timestamp), "'%d-%02i-%02i %02i:%02i:%02i.%06li'", local_time.tm_year + 1900, local_time.tm_mon + 1, local_time.tm_mday, local_time.tm_hour, local_time.tm_min, local_time.tm_sec, nanosecconds);
     
     return timestamp;
-    }
 }
 
 void initQentry(qentry **q){
