c5d5c16bd096917995666f99c17eb6cd9adad784
AnnaLena77 <anna-lena.roth@informatik.hs-fulda.de>
2023-09-04 18:49:44 +0200

    test binary Int-Data with memcpy

--- ompi/mpi/c/init.c
+++ ompi/mpi/c/init.c
@@ -419,13 +419,13 @@ void writeIntoFile(qentry **q){
         buffer[offset] = ',';
         offset ++;
         
-        int procname_len = strlen(item->processorname);
+        /*int procname_len = strlen(item->processorname);
         memcpy(buffer + offset, item->processorname, procname_len);
         offset += procname_len;
         buffer[offset] = ',';
         offset ++;
         
-        /*int processrank_len = strlen(processrank_arr);
+        int processrank_len = strlen(processrank_arr);
         memcpy(buffer + offset, processrank_arr, processrank_len);
         offset ++;
         buffer[offset] = ',';
